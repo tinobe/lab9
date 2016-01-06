95f957e27c34c80d00dd2839bcf86e5ed47375aa
Konstantin Beyer <kobey100@pc06.pool.tp1.uni-duesseldorf.de>
2016-01-06 17:31:13 +0100

    Lab9

--- upwind.cxx
+++ upwind.cxx
@@ -10,17 +10,18 @@ void writeToFile(const double* const u, const string s, const double dx,
                  const double xmin, const int N);
 void initialize(double* const u, const double dx, const double xmin,
                 const int N);
+void step( double* u0, double* u1, const double dx, const double dt, const double v, const int N);
 //---------------------------------------
 int main(){
 
-  const double tEnd = ;
-  const double V = ;
+  const double tEnd = 5;
+  const double V = 1;
 
-  const int N  = ;
+  const int N  = 256;
   const double xmin = -10;
   const double xmax =  10;
   const double dx = (xmax-xmin)/(N-1);
-  double dt = ;
+  double dt =0.5*dx ;
   const int Na = 10; // Number of output files up to tEnd
   const int Nk = int(tEnd/Na/dt);
 
@@ -38,9 +39,9 @@ int main(){
   {
    for(int j=0; j<Nk; j++){
 
-      // Put call to step function here
+     step(u0,u1,dx,dt,V,N); // Put call to step function here
 
-      // swap arrays u0 <-> u1,
+     h=u0; u0=u1; u1=h; // swap arrays u0 <-> u1,
       // however do not copy values, be more clever ;)
    }
    strm.str("");
@@ -78,4 +79,10 @@ void writeToFile(const double* const u, const string s, const double dx,
      out << x << "\t" << u[i] << endl;
    }
    out.close();
+}
+
+void step( double* u0, double* u1, const double dx, const double dt, const double v, const int N){
+	u1[0]=0;u1[N-1]=u1[0];
+	//for(int i=1;i<N;i++) u1[i]=-v*dt/dx*(u0[i]-u0[i-1])+u0[i];
+	for(int i=1;i<N-1;i++) u1[i]=-v*dt/2/dx*(u0[i+1]-u0[i-1])+u0[i];
 }
